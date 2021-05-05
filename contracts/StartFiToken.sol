1ef02e56fb6d1f41020fe0b17b530c16a8fda34e
emanherawy <eman.herawy@gmail.com>
2021-05-06 00:11:07 +0200

    add startfi token

---
+++ contracts/StartFiToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity >=0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+
+
+contract StartfiToken is ERC20PresetFixedSupply{
+    constructor(string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address owner) ERC20PresetFixedSupply(name,symbol,initialSupply,owner)  {
+
+         
+    }
+
+}
