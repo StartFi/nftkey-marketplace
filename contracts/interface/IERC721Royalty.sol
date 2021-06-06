bec56fc4ae014e8e62ef73ef632486a0ff300fe9
emanherawy <eman.herawy@gmail.com>
2021-06-06 13:49:10 +0200

    fix supported infterface and code optomize

--- contracts/interface/IERC721Royalty.sol
+++ contracts/interface/IERC721Royalty.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 interface IERC721Royalty {
  function royaltyInfo(uint256 _tokenId, uint256 _value) external view returns (address issuer, uint256 _royaltyAmount) ;  
 }
